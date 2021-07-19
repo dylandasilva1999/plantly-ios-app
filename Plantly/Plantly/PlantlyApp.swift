25c38b0c7d4a2346c060809b9d3c117680bd51d7
Dylan da Silva <dylandasilva.b@gmail.com>
2021-07-19 14:09:10 +0200

    Initial Project Setup

---
+++ Plantly/Plantly/PlantlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantlyApp.swift
+//  Plantly
+//
+//  Created by Dylan da Silva on 2021/07/19.
+//
+
+import SwiftUI
+
+@main
+struct PlantlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
