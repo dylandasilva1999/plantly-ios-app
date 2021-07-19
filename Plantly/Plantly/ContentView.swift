f6e2fc53e309fa97fed72fc57eb95ea7230db7e9
Dylan da Silva <dylandasilva.b@gmail.com>
2021-07-19 14:35:52 +0200

    Add App Icon & Basic Colours

--- Plantly/Plantly/ContentView.swift
+++ Plantly/Plantly/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack() {
+            Color("Primary Green")
+                .ignoresSafeArea()
+            Text("plantly.")
+                .font(.system(size: 60))
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                .foregroundColor(.white)
+                .padding()
+        }
     }
 }
 
