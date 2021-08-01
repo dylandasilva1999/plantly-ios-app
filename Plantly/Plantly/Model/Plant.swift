273046e5e8cd5cdad34c5a1eecf0d50c599c1e98
Dylan da Silva <dylandasilva.b@gmail.com>
2021-08-01 17:31:15 +0200

    Create Plant Model

---
+++ Plantly/Plantly/Model/Plant.swift
@@ -0,0 +1,18 @@
+//
+//  Plant.swift
+//  Plantly
+//
+//  Created by Dylan da Silva on 2021/08/01.
+//
+
+import SwiftUI
+
+struct Plant: Identifiable {
+    var id = UUID()
+    var name: String
+    var description: String
+    var image: String
+    var type: [String]
+    var propagation: [String]
+    var light: [String]
+}
