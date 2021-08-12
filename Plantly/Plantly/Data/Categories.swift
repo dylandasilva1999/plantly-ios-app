fe7da5698be4af9e497b295eebd691880db4c3ac
Dylan da Silva <dylandasilva.b@gmail.com>
2021-08-12 16:58:31 +0200

    Create Category Model & Categories Data

---
+++ Plantly/Plantly/Data/Categories.swift
@@ -0,0 +1,24 @@
+//
+//  Categories.swift
+//  Plantly
+//
+//  Created by Dylan da Silva on 2021/08/12.
+//
+
+import SwiftUI
+
+let Categories: [Category] = [
+    
+    //Flowering Plants
+    Category(category: "Flowering plants", image: "flower-category"),
+    
+    //Conifers, cycads and allies
+    Category(category: "Conifers, cycads and allies", image: "conifer-category"),
+    
+    //Ferns, and fern allies
+    Category(category: "Ferns, and fern allies", image: "fern-category"),
+    
+    //Mosses and hornworts
+    Category(category: "Mosses and hornworts", image: "moss-category"),
+    
+]
---
+++ Plantly/Plantly/Model/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  Plantly
+//
+//  Created by Dylan da Silva on 2021/08/12.
+//
+
+import SwiftUI
+
+struct Category: Identifiable {
+    var id = UUID()
+    var category: String
+    var image: String
+}
